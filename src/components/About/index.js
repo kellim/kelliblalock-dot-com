8ff11b9709f9afd5e4474c394a59a2b8291ab317
Kelli Blalock <kelli.blalock2@gmail.com>
2019-01-23 00:04:31 -0800

    Update About component and style

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,10 +4,16 @@ import styles from './index.module.scss'
 
 const About = (props) => (
     <div className={styles.about}>
-        <div>
+        <div className={styles.headshot}>
             <Img fluid={props.data.headshotImage.childImageSharp.fluid} alt="Kelli Blalock" />
         </div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pharetra volutpat leo non vestibulum. Praesent pellentesque eros in porttitor consectetur. Sed eget condimentum quam, non egestas sem. Ut faucibus sollicitudin nulla sed porta. Cras elementum elit elit, sit amet suscipit urna volutpat in. Sed volutpat turpis ante, quis tristique ante vulputate et. Etiam consequat mi ac urna egestas sodales et eget mauris. </p>
+        <div className={styles.myInfo}>
+            <h2>About</h2>
+            <p> Hi, I'm Kelli! I love developing web apps with JavaScript and Python. I’m also an 
+                experienced IT professional with a BS degree in Computer Information Systems
+                and I enjoy learning new things. Besides coding, I love writing and sharing what 
+                I've learned with others, so expect to see some blog posts from me soon.</p>
+        </div>
     </div>
  )
 
