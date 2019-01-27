08b42febe32884905c7b31744a3682dec0a9332b
Kelli Blalock <kelli.blalock2@gmail.com>
2019-01-26 16:22:08 -0800

    Remove static query

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,26 +6,13 @@ import Footer from '../components/Footer'
 import './layout.scss'
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
+        <Header />
         <div className="container">
           {children}
         </div>
         <Footer />
       </>
-    )}
-  />
 )
 
 Layout.propTypes = {
