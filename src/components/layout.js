c947ad20865de2d483d775ba816cbf5d006854b6
Kelli Blalock <kelli.blalock2@gmail.com>
2019-01-26 16:46:26 -0800

    Remove StaticQuery import

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import './layout.scss'
